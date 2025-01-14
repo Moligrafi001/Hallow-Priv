4fb1a1ca23bf5282355649df307e4a92c0a462bc
PrismX <148635760+PrismX8@users.noreply.github.com>
2025-01-13 21:46:48 -0500

    Update NinjaLegends.lua

--- games/NinjaLegends.lua
+++ games/NinjaLegends.lua
@@ -1,4 +1,4 @@
--- loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Priv/refs/heads/main/games/NinjaLegends.lua?token=GHSAT0AAAAAAC4PUKQM7GJW5OZP5F7Q4BIEZ4FZWSA",true))()
+-- loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Priv/refs/heads/main/games/NinjaLegends.lua?token=GHSAT0AAAAAAC4PUKQMGWXO2JAJTWMTGI4YZ4F2BDA",true))()
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 local Window = Rayfield:CreateWindow({
     Name = "Hallow Hub | Ninja Legends⭐",
