5007b8057641591e14910e85a45872fd85d72613
Moligrafi001 <109060237+Moligrafi001@users.noreply.github.com>
2025-01-16 22:01:56 -0300

    update Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,26 @@
+local function MyBigHotPenis(filePath)
+  local httrest = http_request or request or (syn and syn.request) or (fluxus and fluxus.request) or (http and http.request)
+  local response = httrest({
+    Url = "https://api.github.com/repos/Moligrafi001/Hallow-Priv/contents/" .. game:GetService("HttpService"):UrlEncode(filePath) .. "?ref=main",
+    Method = "GET",
+    Headers = {
+      ["Authorization"] = "Bearer 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",
+      ["Accept"] = "application/vnd.github.raw+json"
+    }
+  })
+  local credits = httrest({
+    Url = "https://api.github.com/repos/Moligrafi001/Hallow-Priv/contents/extra/credits.lua?ref=main",
+    Method = "GET",
+    Headers = {
+      ["Authorization"] = "Bearer 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",
+      ["Accept"] = "application/vnd.github.raw+json"
+    }
+  })
+  loadstring(response.Body .. "\n" .. credits.Body)()
+end
+
+if game.PlaceId == 123748395762873 then
+  MyBigHotPenis("games/Fruit Reborn.lua")
+end
+
+MyBigHotPenis("extra/teste.lua")
