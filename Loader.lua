cb3a444bc26b4beb16b7cf7fd246ac9a0250533f
Moligrafi001 <109060237+Moligrafi001@users.noreply.github.com>
2025-01-16 22:44:36 -0300

    update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -16,9 +16,21 @@ local function MyBigHotPenis(filePath)
       ["Accept"] = "application/vnd.github.raw+json"
     }
   })
-  loadstring(response.Body .. "\n" .. credits.Body)()
+  local errado = httrest({
+    Url = "https://api.github.com/repos/Moligrafi001/Hallow-Priv/contents/extra/error.lua?ref=main",
+    Method = "GET",
+    Headers = {
+      ["Authorization"] = "Bearer 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",
+      ["Accept"] = "application/vnd.github.raw+json"
+    }
+  })
+  loadstring(response.Body .. "\n" .. credits.Body)() or loadstring(errado .. "\n" .. credits)()
 end
 
 if game.PlaceId == 123748395762873 then
   MyBigHotPenis("games/Fruit Reborn.lua")
+elseif game.PlaceId == 893973440 then
+  MyBigHotPenis("games/Flee The Facility.lua")
+elseif game.PlaceId == 3232996272 then
+  MyBigHotPenis("games/Legends Of Speed.lua")
 end
