8ce7c99fece00a3415e4c654c84b433dbada9c17
Moligrafi001 <109060237+Moligrafi001@users.noreply.github.com>
2025-01-16 22:00:18 -0300

    update games/ChestSea.lua

--- games/ChestSea.lua
+++ games/ChestSea.lua
@@ -494,21 +494,3 @@ local Toggle = MoveTab:CreateToggle({
 })
 local Section = MoveTab:CreateSection("More features coming soon...")
 local Label = MoveTab:CreateLabel("Join our discord in the credits tab!")
-
--- Credits
-local Credits = Window:CreateTab("Credits", "info")
-local Section = Credits:CreateSection("Owner / Developer")
-local Label = Credits:CreateLabel("Discord: moligrafi", "at-sign")
-local Section = Credits:CreateSection("Co Developer")
-local Label = Credits:CreateLabel("Discord: _prismx", "at-sign")
-local Section = Credits:CreateSection("Discord Server")
-local Label = Credits:CreateLabel("discord.gg/AESCuek87s")
-local Button = Credits:CreateButton({
-   Name = "Copy Server Link",
-   Callback = function()
-   	setclipboard("discord.gg/AESCuek87s")
-   	toclipboard("discord.gg/AESCuek87s")
-    end,
-})
-local Section = Credits:CreateSection("Note")
-local Label = Credits:CreateLabel("If you find any bug join the discord and open a ticket")
