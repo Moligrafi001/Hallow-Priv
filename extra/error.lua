67c49063c448ea5d579a2da1a3ab6f7d2bcd62b2
Moligrafi001 <109060237+Moligrafi001@users.noreply.github.com>
2025-01-16 23:23:40 -0300

    update extra/error.lua

---
+++ extra/error.lua
@@ -0,0 +1,8 @@
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+local Window = Rayfield:CreateWindow({
+   Name = "Hallow Hub | No Game Dettected",
+   Icon = 17091459839,
+   LoadingTitle = "Hallow Hub",
+   LoadingSubtitle = "By Moligrafi",
+   Theme = "Amethyst"
+})
