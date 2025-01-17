047c0ed4cf8b2cde6c5da1c8760dc5abe1c9e242
Moligrafi001 <109060237+Moligrafi001@users.noreply.github.com>
2025-01-16 21:46:23 -0300

    update extra/credits.lua

--- extra/credits.lua
+++ extra/credits.lua
@@ -1,1 +1,17 @@
+-- loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/Hallow-Hub/main/extra/Credits.lua",true))()
 
+-- Credits.
+local Credits = Window:CreateTab("Credits", "info")
+local Section = Credits:CreateSection("Creator")
+local Label = Credits:CreateLabel("Discord: moligrafi", "at-sign")
+local Section = Credits:CreateSection("Co-Dev")
+local Label = Credits:CreateLabel("Discord: world_star_hacker", "at-sign")
+local Section = Credits:CreateSection("Discord Server")
+local Label = Credits:CreateLabel("discord.gg/AESCuek87s")
+local Button = Credits:CreateButton({
+   Name = "Copy Server Link",
+   Callback = function()
+   	setclipboard("discord.gg/AESCuek87s")
+   	toclipboard("discord.gg/AESCuek87s")
+    end,
+})
