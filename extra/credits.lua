4c7c3bf7d3a00a452512bc5a5ab5fce3d1a37f8f
Moligrafi001 <109060237+Moligrafi001@users.noreply.github.com>
2025-01-16 21:54:58 -0300

    update extra/credits.lua

--- extra/credits.lua
+++ extra/credits.lua
@@ -1,15 +1,17 @@
 -- Credits
 local Credits = Window:CreateTab("Credits", "info")
-local Section = Credits:CreateSection("Creator")
-local Label = Credits:CreateLabel("Discord: moligrafi", "at-sign")
-local Section = Credits:CreateSection("Co-Developer")
-local Label = Credits:CreateLabel("Discord: _prismx", "at-sign")
-local Section = Credits:CreateSection("Discord Server")
-local Label = Credits:CreateLabel("discord.gg/AESCuek87s")
-local Button = Credits:CreateButton({
-   Name = "Copy Server Link",
-   Callback = function()
-   	setclipboard("discord.gg/AESCuek87s")
-   	toclipboard("discord.gg/AESCuek87s")
-    end,
+local FounderSection = Pussy:CreateSection("Founder Developer")
+local Label = Pussy:CreateLabel("Discord: moligrafi", "at-sign")
+local DevSection = Pussy:CreateSection("Co-Developer")
+local Label = Pussy:CreateLabel("Discord: _prismx", "at-sign")
+local ServerSection = Pussy:CreateSection("Discord Server")
+local Label = Pussy:CreateLabel("discord.gg/AESCuek87s")
+local ButtonServer = Pussy:CreateButton({
+  Name = "Copy Server Link",
+  Callback = function()
+    setclipboard("discord.gg/XnsG57tQWS")
+    toclipboard("discord.gg/XnsG57tQWS")
+  end,
 })
+local NoteSection = Pussy:CreateSection("Note")
+local NoteLabel = Pussy:CreateLabel("If you find any bug join the discord and open a ticket")
