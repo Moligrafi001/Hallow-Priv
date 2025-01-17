497eea8bb938779c03594d81ee0c4821e923a895
Moligrafi001 <109060237+Moligrafi001@users.noreply.github.com>
2025-01-16 21:57:46 -0300

    update extra/teste.lua

---
+++ extra/teste.lua
@@ -0,0 +1,12 @@
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+local Window = Rayfield:CreateWindow({
+   Name = "Hallow Hub | Fruit Reborn",
+   Icon = 17091459839,
+   LoadingTitle = "Hallow Hub",
+   LoadingSubtitle = "By Moligrafi",
+   Theme = "Amethyst"
+})
+
+-- Menu		
+local Menu = Window:CreateTab("Main", "home")
+local Section = Menu:CreateSection("Mob Farm")
