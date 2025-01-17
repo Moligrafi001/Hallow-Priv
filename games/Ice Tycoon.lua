2a17cb63e63fe8867961e4b57225eb9900bc190d
Moligrafi001 <109060237+Moligrafi001@users.noreply.github.com>
2025-01-17 00:09:00 -0300

    update games/Ice Tycoon.lua

--- games/Ice Tycoon.lua
+++ games/Ice Tycoon.lua
@@ -424,19 +424,3 @@ local Toggle = MoveTab:CreateToggle({
    	InfJump()
    end,
 })
-
--- Credits
-local Credits = Window:CreateTab("Credits", "info")
-local Section = Credits:CreateSection("Creator")
-local Label = Credits:CreateLabel("Discord: moligrafi", "at-sign")
-local Section = Credits:CreateSection("Discord Server")
-local Label = Credits:CreateLabel("discord.gg/AESCuek87s")
-local Button = Credits:CreateButton({
-   Name = "Copy Server Link",
-   Callback = function()
-   	setclipboard("discord.gg/AESCuek87s")
-   	toclipboard("discord.gg/AESCuek87s")
-    end,
-})
-local Section = Credits:CreateSection("Note")
-local Label = Credits:CreateLabel("If you find any bug join the discord and open a ticket")
