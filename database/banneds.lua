79f479ff6bb31208aacddc7ee3b807ad96fa4de8
Moligrafi001 <109060237+Moligrafi001@users.noreply.github.com>
2025-01-18 03:14:23 -0300

    update database/banneds.lua

--- database/banneds.lua
+++ database/banneds.lua
@@ -3,3 +3,5 @@ local banneds = {"7862872050", "456", "789"}
 if game.Players.LocalPlayer.UserId == unpack(banneds) then
   game.Players.LocalPlayer:Kick("\nYou are banned from Hallow Hub!\nJoin the discord below to appeal.")
 end
+
+print("sim")
