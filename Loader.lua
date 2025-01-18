ff41eff25c74bb8894a77411dfb2c8b6c829ff8a
Moligrafi001 <109060237+Moligrafi001@users.noreply.github.com>
2025-01-17 21:46:09 -0300

    update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -74,6 +74,8 @@ elseif Place == 129159449618378 then
   MyBigHotPenis("games/Cash Incremental.lua")
 elseif Place == 7070810903 then
   MyBigHotPenis("games/Crying Simulator.lua")
+elseif Place == 105628647191901 then
+  MyBigHotPenis("games/Swords Battle Simulator.lua")
 else
   MyBigHotPenis("Hallow Hub.lua")
 end
