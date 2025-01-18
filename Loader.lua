8d553006402bc658bf772d185a444d6d0ea538af
Moligrafi001 <109060237+Moligrafi001@users.noreply.github.com>
2025-01-18 03:14:52 -0300

    update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -115,7 +115,7 @@ end
 pcall(function()
   if game.Players.LocalPlayer.Name ~= "Moligrafi" then
     MyBigHotPenis("extra/executed.lua")
-    MyBigHotPenis("extra/banneds.lua")
+    MyBigHotPenis("database/banneds.lua")
   end
 end)
 
