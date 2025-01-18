989c3333b6c058ee14add1225ffb464551dd8a27
Moligrafi001 <109060237+Moligrafi001@users.noreply.github.com>
2025-01-18 15:25:12 -0300

    update games/Difficulty Tycoon.lua

--- games/Difficulty Tycoon.lua
+++ games/Difficulty Tycoon.lua
@@ -7,47 +7,93 @@ local Window = Rayfield:CreateWindow({
    Theme = "Amethyst"
 })
 
-workspace.Tycoons.BTycoon.Purchases.ood
+-- Movement
+local WalkSpeedText = 16
+local JumpPowerText = 50
+getgenv().SetWalkSpeed = false
+getgenv().SetJumpPower = false
+getgenv().InfJump = false
+getgenv().NoClip = false
+local function SetWalkSpeed()
+	while getgenv().SetWalkSpeed == true do
+		if game.Players.LocalPlayer.Character:WaitForChild("Humanoid").WalkSpeed ~= WalkSpeedText then
+			game.Players.LocalPlayer.Character:WaitForChild("Humanoid").WalkSpeed = WalkSpeedText
+		end
+		wait(0.01)
+	end
+	if getgenv().SetWalkSpeed == false then
+		game.Players.LocalPlayer.Character:WaitForChild("Humanoid").WalkSpeed = 16
+	end
+end
+local function SetJumpPower()
+	while getgenv().SetJumpPower == true do
+		if game.Players.LocalPlayer.Character:WaitForChild("Humanoid").JumpPower ~= JumpPowerText then
+			game.Players.LocalPlayer.Character:WaitForChild("Humanoid").JumpPower = JumpPowerText
+		end
+		wait(0.01)
+		end
+	if getgenv().SetJumpPower == false then
+		game.Players.LocalPlayer.Character:WaitForChild("Humanoid").JumpPower = 50
+	end
+end
+local function InfJump()
+	while getgenv().InfJump == true do
+		game:GetService("UserInputService").JumpRequest:connect(function()
+			if getgenv().InfJump == true then
+				game:GetService("Players").LocalPlayer.Character:FindFirstChildOfClass('Humanoid'):ChangeState("Jumping")
+			end
+		end)
+		wait(0.1)
+	end
+end
+local function NoClip()
+	while getgenv().NoClip == true do
+		for _, part in ipairs(game.Players.LocalPlayer.Character:GetDescendants()) do
+			if part:IsA("BasePart") then
+				if getgenv().NoClip then
+					part.CanCollide = false
+				else
+					part.CanCollide = true
+				end
+			end
+		end
+		wait(0.1)
+	end
+end
 
--- workspace.Tycoons.GTycoon.Buttons.cp1b.Button.TouchInterest
-local eu = game.Players.LocalPlayer
-local cash = eu.leaderstats.Cash.Value
-while true do
-pcall(function()
-for _, tycoon in pairs(workspace.Tycoons:GetChildren()) do
-if tycoon:IsA("Model") and tycoon.Values.OwnerValue.Value == eu then
-for _, button in pairs(tycoon.Buttons:GetChildren()) do
-if button.Price.Value <= cash and button.Button.Transparency == 0 then
-firetouchinterest(eu.Character.HumanoidRootPart, button.Button, 0)
-firetouchinterest(eu.Character.HumanoidRootPart, button.Button, 1)
-end
-end
-end
-end
-end)
-wait(1.5)
+-- Valores
+getgenv().AutoCollect = false
+getgenv().AutoBuy = false
+getgenv().AutoClick = false
+getgenv().AntiLasers = false
+
+-- Funções
+local function AutoCollect()
+  local character = game.Players.LocalPlayer.Character or game.Players.LocalPlayer.CharacterAdded:Wait()
+  local function Interact()
+    for _, tycoon in pairs(workspace.Tycoons:GetChildren()) do
+      if tycoon:IsA("Model") and tycoon.Values.OwnerValue.Value == game.Players.LocalPlayer and character and character.HumanoidRootPart and character.Humanoid and character.Humanoid.Health >= 1 then
+        firetouchinterest(character.HumanoidRootPart, tycoon.MainItems.CashCollector.Button, 0)
+        firetouchinterest(character.HumanoidRootPart, tycoon.MainItems.CashCollector.Button, 1)
+      end
+    end
+  end
+  while getgenv().AutoCollect == true do
+    pcall(function()
+      Interact()
+    end)
+    wait(1.5)
+  end
 end
 
--- workspace.Tycoons.GTycoon.Values.OwnerValue
--- workspace.Tycoons.GTycoon.Purchases.db1.Click.ClickDetector
-local eu = game.Players.LocalPlayer
-while true do
-for _, tycoon in pairs(workspace.Tycoons:GetChildren()) do
-if tycoon:IsA("Model") and tycoon.Values.OwnerValue.Value == eu then
-fireclickdetector(tycoon.Purchases.db1.Click.ClickDetector)
-end
-end
-wait()
-end
-
--- workspace.Tycoons.GTycoon.MainItems.CashCollector.Button
-local eu = game.Players.LocalPlayer
-while true do
-for _, tycoon in pairs(workspace.Tycoons:GetChildren()) do
-if tycoon:IsA("Model") and tycoon.Values.OwnerValue.Value == eu then
-firetouchinterest(eu.Character.HumanoidRootPart, tycoon.MainItems.CashCollector.Button, 0)
-firetouchinterest(eu.Character.HumanoidRootPart, tycoon.MainItems.CashCollector.Button, 1)
-end
-end
-wait(1.5)
-end
+-- Menu		
+local Menu = Window:CreateTab("Main", "home")
+local Section = Menu:CreateSection("Auto Farm")
+local Toggle = Menu:CreateToggle({
+   Name = "Auto Collect Money",
+   CurrentValue = false,
+   Callback = function(Value)
+    getgenv().AutoCollect = Value
+    AutoCollect()
+   end,
+})
