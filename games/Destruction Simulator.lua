05724f8e9c7bf1f6589674894e10b3f209db4240
PrismX <148635760+PrismX8@users.noreply.github.com>
2025-01-21 12:57:53 -0500

    Create Destruction Simulator.lua

---
+++ games/Destruction Simulator.lua
@@ -0,0 +1,243 @@
+-- loadstring(game:HttpGet(''))()
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+
+local Window = Rayfield:CreateWindow({
+    Name = "HallowHub | Destruction Simulator",
+    Icon = 17091459839,
+    LoadingTitle = "HallowHub",
+    LoadingSubtitle = "By Moligrafi",
+    Theme = "Amethyst"
+})
+
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local player = game.Players.LocalPlayer
+local lplr = game:GetService("Players").LocalPlayer
+local rstorage = game:GetService("ReplicatedStorage")
+local workspace = game:GetService("Workspace")
+
+-- Movement
+local WalkSpeedText = 16
+local JumpPowerText = 50
+_G.SetWalkSpeed = false
+_G.SetJumpPower = false
+_G.InfJump = false
+_G.NoClip = false
+local function SetWalkSpeed()
+	while _G.SetWalkSpeed == true do
+		if game.Players.LocalPlayer.Character:WaitForChild("Humanoid").WalkSpeed ~= WalkSpeedText then
+			game.Players.LocalPlayer.Character:WaitForChild("Humanoid").WalkSpeed = WalkSpeedText
+		end
+		wait(0.01)
+	end
+	if _G.SetWalkSpeed == false then
+		game.Players.LocalPlayer.Character:WaitForChild("Humanoid").WalkSpeed = 16
+	end
+end
+local function SetJumpPower()
+	while _G.SetJumpPower == true do
+		if game.Players.LocalPlayer.Character:WaitForChild("Humanoid").JumpPower ~= JumpPowerText then
+			game.Players.LocalPlayer.Character:WaitForChild("Humanoid").JumpPower = JumpPowerText
+		end
+		wait(0.01)
+		end
+	if _G.SetJumpPower == false then
+		game.Players.LocalPlayer.Character:WaitForChild("Humanoid").JumpPower = 50
+	end
+end
+local function InfJump()
+	while _G.InfJump == true do
+		game:GetService("UserInputService").JumpRequest:connect(function()
+			if _G.InfJump == true then
+				game:GetService("Players").LocalPlayer.Character:FindFirstChildOfClass('Humanoid'):ChangeState("Jumping")
+			end
+		end)
+		wait(0.1)
+	end
+end
+local function NoClip()
+	while _G.NoClip == true do
+		for _, part in ipairs(game.Players.LocalPlayer.Character:GetDescendants()) do
+			if part:IsA("BasePart") then
+				if _G.NoClip then
+					part.CanCollide = false
+				else
+					part.CanCollide = true
+				end
+			end
+		end
+		wait(0.1)
+	end
+end
+
+local windowTab = Window:CreateTab("Main", "medal")
+local section = windowTab:CreateSection("OP auto coin", true)
+
+local isAutoAddingCoins = false
+
+local coin = windowTab:CreateToggle({
+    Name = "Auto Coin (OP)",
+    Callback = function()
+
+        isAutoAddingCoins = not isAutoAddingCoins
+
+        if isAutoAddingCoins then
+            while isAutoAddingCoins do
+                local Event = ReplicatedStorage.Remotes.generateBoost
+                Event:FireServer("Coins", 999999, 18959994)  -- Automatically add 1,000,000 coins
+                wait(0.1)  -- Wait for 0.1 seconds before firing again
+            end
+        end
+    end
+})
+local section = windowTab:CreateSection("Area Destroyer", true)
+
+local AddLevelButton = windowTab:CreateButton({
+    Name = "Explode Area You Are Standing In (must be in a area)",
+    Callback = function()
+        local launcher = lplr.Character:FindFirstChild("Launcher") or lplr.Backpack:FindFirstChild("Launcher")
+        
+        if not launcher then
+            Rayfield:Notify({
+                Title = "Error",
+                Content = "No launcher found in your inventory or equipped!",
+                Duration = 3,
+                Type = "Error"
+            })
+            return
+        end
+
+        local currentAreaName = lplr:FindFirstChild("CurrentArea") and lplr.CurrentArea.Value
+        if not currentAreaName then
+            Rayfield:Notify({
+                Title = "Error",
+                Content = "CurrentArea value not found for the player!",
+                Duration = 3,
+                Type = "Error"
+            })
+            return
+        end
+
+        local area = workspace:FindFirstChild("Areas"):FindFirstChild(currentAreaName)
+        if not area then
+            Rayfield:Notify({
+                Title = "Not Standing in an area",
+                Content = "You MUST be standing in a area for this to work",
+                Duration = 3,
+                Type = "Error"
+            })
+            return
+        end
+
+        local rstorage = game:GetService("ReplicatedStorage")
+
+        for _, v in pairs(area:GetDescendants()) do
+            if v:IsA("BasePart") then
+                rstorage.Remotes.explodeRocket:FireServer(tick(), launcher.Stats, v.Position, launcher.Assets.Rocket.Boom)
+            end
+        end
+
+        Rayfield:Notify({
+            Title = "Success",
+            Content = "Exploded all parts in " .. currentAreaName .. "!",
+            Duration = 3,
+            Type = "Success"
+        })
+    end
+})
+
+
+local RankUpButton = windowTab:CreateButton({
+    Name = "Rank Up (must be max level)",
+    Callback = function()
+        game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("rankUp"):FireServer()
+    end
+})
+
+local GiveAllBoostsButton = windowTab:CreateButton({
+    Name = "Get All Boosts",
+    Callback = function()
+        local boosts = {
+
+            { "BrickBoost", 180, 2 },
+
+            { "CoinBoost", 180, 2 },
+
+            { "Coins", 999999, 1000000 },
+
+            { "ExtraSpeed", 180, 2 },
+
+            { "Levels", 480, 55 },
+
+            { "SuperJump", 180, 2 },
+
+            { "XPBoost", 180, 2 }
+        }
+
+        for _, args in ipairs(boosts) do
+            game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("generateBoost"):FireServer(unpack(args))
+        end
+    end
+})
+
+local Menu = Window:CreateTab("Player", "person-standing")
+
+local Section = Menu:CreateSection("Player", "person-standing")
+
+local Input = Menu:CreateInput({
+   Name = "Player Walk Speed",
+   CurrentValue = "16",
+   Flag = "WalkSpeedInput",
+   PlaceholderText = "Default Walk Speed = 16",
+   RemoveTextAfterFocusLost = false,
+   Callback = function(Text)
+   	WalkSpeedText = Text
+   end,
+})
+local Toggle = Menu:CreateToggle({
+   Name = "Toggle Walk Speed",
+   CurrentValue = false,
+   Flag = "WalkSpeedToggle", 
+   Callback = function(Value)
+   	_G.SetWalkSpeed = Value
+   	SetWalkSpeed()
+   end,
+})
+local Toggle = Menu:CreateToggle({
+   Name = "No Clip",
+   CurrentValue = false,
+   Flag = "NoClipToggle",
+   Callback = function(Value)
+   	_G.NoClip = Value
+   	NoClip()
+   end,
+})
+local Section = Menu:CreateSection("Jump")
+local Input = Menu:CreateInput({
+   Name = "Player Jump Power",
+   CurrentValue = "",
+   Flag = "JumpPowerInput",
+   PlaceholderText = "Default Jump Power = 50",
+   RemoveTextAfterFocusLost = false,
+   Callback = function(Text)
+   	JumpPowerText = Text
+   end,
+})
+local Toggle = Menu:CreateToggle({
+   Name = "Toggle Jump Power",
+   CurrentValue = false,
+   Flag = "JumpPowerToggle",
+   Callback = function(Value)
+   	_G.SetJumpPower = Value
+   	SetJumpPower()
+   end,
+})
+local Toggle = Menu:CreateToggle({
+   Name = "Inf Jump",
+   CurrentValue = false,
+   Flag = "InfJumpToggle",
+   Callback = function(Value)
+   	_G.InfJump = Value
+   	InfJump()
+   end,
+})
+
