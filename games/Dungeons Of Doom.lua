772cc1c69b2169398a3dbf292002b4dc946bed7e
Moligrafi <109060237+Moligrafi001@users.noreply.github.com>
2025-03-08 04:15:48 -0300

    update games/Dungeons Of Doom.lua

--- games/Dungeons Of Doom.lua
+++ games/Dungeons Of Doom.lua
@@ -521,19 +521,3 @@ local Toggle = MoveTab:CreateToggle({
    	NoClip()
    end,
 })
-
--- Credits.
-local Credits = Window:CreateTab("Credits", "info")
-local Section = Credits:CreateSection("Creator")
-local Label = Credits:CreateLabel("Discord: moligrafi", "at-sign")
-local Section = Credits:CreateSection("Co-Dev")
-local Label = Credits:CreateLabel("Discord: world_star_hacker", "at-sign")
-local Section = Credits:CreateSection("Discord Server")
-local Label = Credits:CreateLabel("discord.gg/AESCuek87s")
-local Button = Credits:CreateButton({
-   Name = "Copy Server Link",
-   Callback = function()
-       setclipboard("discord.gg/AESCuek87s")
-       toclipboard("discord.gg/AESCuek87s")
-    end,
-})
