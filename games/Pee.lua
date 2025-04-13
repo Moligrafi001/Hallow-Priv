4eba9c2b0128193c248f4f1623f2174006f56cb4
Moligrafi <109060237+Moligrafi001@users.noreply.github.com>
2025-04-13 20:23:13 -0300

    update games/Pee.lua

--- games/Pee.lua
+++ games/Pee.lua
@@ -9,36 +9,59 @@ local Window = Rayfield:CreateWindow({
 
 -- Locais
 local Settings = {
-  Selected = "Nenhum"
+  Free = "Hammer",
+  VIP = "PeeC4"
 }
 
 -- Funções
 local function RemoveTool()
   game:GetService("ReplicatedStorage").remote.removeTool:InvokeServer()
 end
-local function GetTool()
-  if Settings.Selected == "Nenhum" then
-    
-  else
-    game:GetService("ReplicatedStorage").remote.giveTool:InvokeServer(Settings.Selected)
+local function GetTool(type)
+  if type == "Free" then
+    game:GetService("ReplicatedStorage").remote.giveTool:InvokeServer(Settings.Free)
+  elseif type == "VIP" then
+    game:GetService("ReplicatedStorage").remote.giveTool:InvokeServer(Settings.VIP)
   end
 end
 
 local Menu = Window:CreateTab("Menu", "home")
-Section = Menu:CreateSection("Get Items")
+Section = Menu:CreateSection("Get Free Items")
 Dropdown = Menu:CreateDropdown({
    Name = "Selected Item",
    Options = {"Hammer", "PeeBottle", "Crowbar", "PeeBomb", "Gun"},
-   CurrentOption = "Select an Item",
+   CurrentOption = Settings.Free,
    MultipleOptions = false,
    Callback = function(Options)
-   		Settings.Selected = Options[1]
+   		Settings.Free = Options[1]
    end,
 })
 Button = Menu:CreateButton({
    Name = "Get Item",
    Callback = function()
-   	GetTool()
+   	GetTool("Free")
+   end,
+})
+Button = Menu:CreateButton({
+   Name = "Remove Item",
+   Callback = function()
+   	RemoveTool()
+   end,
+})
+Section = Menu:CreateSection("Get VIP Items")
+Dropdown = Menu:CreateDropdown({
+   Name = "Selected Item",
+   Options = {"PeeAbility", "PeeC4", "PaintBucket", "PeeGun"},
+   CurrentOption = Settings.VIP,
+   MultipleOptions = false,
+   Callback = function(Options)
+   		Settings.VIP = Options[1]
+   end,
+})
+Button = Menu:CreateButton({
+   Name = "Get Item",
+   Callback = function()
+   	GetTool("VIP")
    end,
 })
 Button = Menu:CreateButton({
