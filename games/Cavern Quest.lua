1b9154171e8dcec50ab28c36495e6d2eabcf0ae9
Moligrafi <109060237+Moligrafi001@users.noreply.github.com>
2025-04-23 16:54:35 -0300

    update games/Cavern Quest.lua

--- games/Cavern Quest.lua
+++ games/Cavern Quest.lua
@@ -9,9 +9,11 @@ local Window = Rayfield:CreateWindow({
 
 -- Valores
 getgenv().KillAura = false
+getgenv().MobsESP = false
 
 -- Locais
 local eu = game:GetService("Players").LocalPlayer
+local ColorESP = Color3.fromRGB(255, 125, 0)
 
 -- Funções
 local function KillAura()
@@ -22,6 +24,30 @@ local function KillAura()
         table.insert(Targets, npc)
       end
       eu.Character.RE:FireServer("UseSkill", { ComboIndex = 1, UUID = "{9}", Slot = "NormalSkill", TargetList = Targets, SkillId = "SC_normal_sword_1" })
+    end)
+  end
+end
+local function MobsESP()
+  while getgenv().MobsESP and task.wait(0.1) do
+    pcall(function()
+      for _, npc in pairs(workspace.Npc:GetChildren()) do
+        if npc.Highlight.Enabled == true then
+          if npc.Highlight.FillColor ~= ColorESP or npc.Highlight.OutlineColor ~= ColorESP then
+            npc.Highlight.FillColor, npc.Highlight.OutlineColor = ColorESP, ColorESP
+          end
+        else
+          npc.Highlight.Enabled = true
+        end
+      end
+    end)
+  end
+  if not getgenv().MobsESP then
+    pcall(function()
+      for _, npc in pairs(workspace.Npc:GetChildren()) do
+        if npc.Highlight.Enabled == true then
+          npc.Highlight.Enabled = false
+        end
+      end
     end)
   end
 end
@@ -36,4 +62,20 @@ Toggle =  Menu:CreateToggle({
    	getgenv().KillAura = Value
    	KillAura()
    end,
+})
+Section = Menu:CreateSection("Visual")
+Toggle =  Menu:CreateToggle({
+   Name = "ESP Enemies",
+   CurrentValue = false,
+   Callback = function(Value)
+   	getgenv().MobsESP = Value
+   	MobsESP()
+   end,
+})
+ColorPicker = Menu:CreateColorPicker({
+   Name = "Highlight Color",
+   Color = ColorESP,
+   Callback = function(Value)
+  	ColorESP = Value
+   end
 })
