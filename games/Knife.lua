860fc7d8a66dcca57e84b02ad3dea6c7a3293211
Moligrafi <109060237+Moligrafi001@users.noreply.github.com>
2025-04-24 12:28:06 -0300

    Delete the file at 'games/Knife.lua'

--- games/Knife.lua
+++
@@ -1,8 +0,0 @@
-local args = {
-    [1] = "Swing"
-}
-
-game:GetService("ReplicatedStorage").Remotes.Core:FireServer(unpack(args))
-workspace.Map.currentMap.Water
-workspace.Map.PyramidTeleport.Lava
-workspace.Map.PyramidTeleport.Finish
