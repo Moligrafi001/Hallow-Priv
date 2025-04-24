e9643743ae51c2112ebabf1196c63ca9d9bd3dad
Moligrafi <109060237+Moligrafi001@users.noreply.github.com>
2025-04-24 12:01:07 -0300

    update games/Knife.lua

---
+++ games/Knife.lua
@@ -0,0 +1,8 @@
+local args = {
+    [1] = "Swing"
+}
+
+game:GetService("ReplicatedStorage").Remotes.Core:FireServer(unpack(args))
+workspace.Map.currentMap.Water
+workspace.Map.PyramidTeleport.Lava
+workspace.Map.PyramidTeleport.Finishq
