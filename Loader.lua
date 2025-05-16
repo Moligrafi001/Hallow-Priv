c07cd4c46bd955249b416c2c7e6b0ddb58754e08
Moligrafi <109060237+Moligrafi001@users.noreply.github.com>
2025-05-16 17:57:53 -0300

    update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -249,6 +249,8 @@ elseif Game == 4991821731 then
   MyBigHotPenis("games/PETAPETA.lua")
 elseif Game == 73885730 then
   MyBigHotPenis("games/PrisonLife.lua")
+elseif Game == 7355857660 then
+  MyBigHotPenis("games/Skinwalkers.lua")
 else
   MyBigHotPenis("Hallow Hub.lua")
 end
