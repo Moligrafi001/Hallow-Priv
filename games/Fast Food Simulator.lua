215d48563851b8ee5005cf75ba0a9bf320f8343e
Moligrafi <109060237+Moligrafi001@users.noreply.github.com>
2025-05-17 10:15:44 -0300

    update games/Fast Food Simulator.lua

---
+++ games/Fast Food Simulator.lua
@@ -0,0 +1,22 @@
+local Rayfield = loadstring(game:HttpGet("https://sirius.menu/rayfield"))()
+local Window = Rayfield:CreateWindow({
+  Name = "Hallow Hub | Fast Food Simulator",
+  Icon = 17091459839,
+  LoadingTitle = "Hallow Hub",
+  LoadingSubtitle = "By Moligrafi",
+  Theme = "Amethyst"
+})
+
+
+-- Global Values
+getgenv().AutoDirty = false
+
+-- Locals
+local eu = game:GetService("Players").LocalPlayer
+
+-- Functions
+local function AutoDirty()
+  while getgenv().AutoDirty and task.wait(1) do
+    
+  end
+end
