96ec92613cc888dfbf6c86e334361cc125801294
Moligrafi <109060237+Moligrafi001@users.noreply.github.com>
2025-05-17 10:28:24 -0300

    update games/Fast Food Simulator.lua

--- games/Fast Food Simulator.lua
+++ games/Fast Food Simulator.lua
@@ -16,17 +16,21 @@ local eu = game:GetService("Players").LocalPlayer
 
 -- Functions
 local function AutoDirty()
-  while getgenv().AutoDirty and task.wait(1) do
-    if not GetAttribute("Connected") then
-    end
+  if not workspace.OwnedRestaurants[eu.Name].DirtyTrays:GetAttribute("Connected") then
+    workspace.OwnedRestaurants[eu.Name].DirtyTrays:SetAttribute("Connected")
+    workspace.OwnedRestaurants[eu.Name].DirtyTrays.ChildAdded:Connect(function(instance)
+      if getgenv().AutoDirty then
+        instance.Collect:FireServer()
+      end
+    end)
   end
 end
 
-workspace.OwnedRestaurants.HallowHubby.DirtyTrays:FindFirstChild("Dirty Tray").Collect:FireServer()
-local args = {
-    [1] = workspace.OwnedRestaurants.HallowHubby.Ingredients:FindFirstChild("{aea78d64-214e-4190-8386-0ce3b6e8737e}")
-}
+-- workspace.OwnedRestaurants.HallowHubby.DirtyTrays:FindFirstChild("Dirty Tray").Collect:FireServer()
+-- local args = {
+--     [1] = workspace.OwnedRestaurants.HallowHubby.Ingredients:FindFirstChild("{aea78d64-214e-4190-8386-0ce3b6e8737e}")
+-- }
 
-game:GetService("ReplicatedStorage").Remotes.Gameplay.TakeIngredient:FireServer(unpack(args))
-workspace.OwnedRestaurants.HallowHubby.Furniture.CashRegisters.Register.TakeOrder:FireServer()
-workspace.OwnedRestaurants.HallowHubby.Trash.Tray.FoodsLeft.Burger["Cooked Patty"]
+-- game:GetService("ReplicatedStorage").Remotes.Gameplay.TakeIngredient:FireServer(unpack(args))
+-- workspace.OwnedRestaurants.HallowHubby.Furniture.CashRegisters.Register.TakeOrder:FireServer()
+-- workspace.OwnedRestaurants.HallowHubby.Trash.Tray.FoodsLeft.Burger["Cooked Patty"]
