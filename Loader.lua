e7e03891ad245b277dc667a40fd7c3f0b8f4d2c8
PrismX <148635760+PrismX8@users.noreply.github.com>
2025-05-17 23:03:24 -0400

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -265,7 +265,7 @@ elseif Game == 7513130835 then
   MyBigHotPenis("games/Untitled Drill Game.lua")
 elseif Game == 7435020231 then
   MyBigHotPenis("games/Dead Spells.lua")
-elseif Game == 81440632616906 then
+elseif Game == 8144063261 then
   MyBigHotPenis("games/DigToEarthsCore.lua")
 else
   MyBigHotPenis("Hallow Hub.lua")
