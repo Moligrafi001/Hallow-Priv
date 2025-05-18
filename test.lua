965457c2a6ccac1aaec345af931711f71a20454b
Moligrafi <109060237+Moligrafi001@users.noreply.github.com>
2025-05-18 13:26:21 -0300

    update test.lua

--- test.lua
+++ test.lua
@@ -1,11 +1,1 @@
-while task.wait(1) do
-pcall(function()
-for _, p in pairs(game.Players:GetPlayers()) do
-local args = {
-    [1] = p.Character
-}
-
-game:GetService("ReplicatedStorage").Remotes.Heal:FireServer(unpack(args))
-end
-end)
-end
+print("Test working")
