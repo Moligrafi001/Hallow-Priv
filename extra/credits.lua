f8a492d5e6685aef04617f4df83a920103401278
PrismX <148635760+PrismX8@users.noreply.github.com>
2025-05-17 23:08:25 -0400

    Update credits.lua

--- extra/credits.lua
+++ extra/credits.lua
@@ -1,8 +1,7 @@
 -- Credits
 local Pussy = Window:CreateTab("Credits", "info")
-local FounderSection = Pussy:CreateSection("Founder Developer")
+local FounderSection = Pussy:CreateSection("Founder Developers")
 local Label = Pussy:CreateLabel("Discord: moligrafi", "at-sign")
-local DevSection = Pussy:CreateSection("Co-Developer")
 local Label = Pussy:CreateLabel("Discord: _prismx", "at-sign")
 local ServerSection = Pussy:CreateSection("Discord Server")
 local Label = Pussy:CreateLabel("discord.gg/AESCuek87s")
