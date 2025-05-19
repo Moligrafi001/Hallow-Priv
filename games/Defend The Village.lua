a3f4afca365fbab655633132e8f19e9e78fe9e2d
Moligrafi <109060237+Moligrafi001@users.noreply.github.com>
2025-05-18 21:25:03 -0300

    update games/Defend The Village.lua

--- games/Defend The Village.lua
+++ games/Defend The Village.lua
@@ -1,20 +1,38 @@
-local args = {
-    [1] = "M1911",
-    [2] = CFrame.new(-99.67828369140625, 6.019713401794434, -230.1353759765625) * CFrame.Angles(-0.04435689374804497, 0.4749751687049866, 0.020295660942792892),
-    [3] = {
-        [1] = {
-            ["id"] = "135"
-        }
-    }
-}
+getgenv().AutoUpgrade = false
 
-game:GetService("ReplicatedStorage").Networking.Remotes.Weapons.GunFired:FireServer(unpack(args))
-workspace.Things.Zombies
-for _, zombie in pairs(workspace.Things.Zombies:GetChildren()) do
+local function KillAll()
+local function AutoKill()
+  while getgenv().AutoKill and task.wait(1) do
+    if not workspace.Things.Zombies:GetAttribute("Connected") then
+      workspace.Things.Zombies:SetAttribute("Connected", true)
+      workspace.Things.Zombies.ChildAdded:Connect(function(instance)
+        if getgenv().AutoKill then
+          repeat
+            game:GetService("ReplicatedStorage").Networking.Remotes.Weapons.GunFired:FireServer("M1911", instance.HumanoidRootPart.CFrame * CFrame.Angles(-0.04435689374804497, 0.4749751687049866, 0.020295660942792892), {{ id = tonumber(instance.name) }})
+            task.wait(0.05)
+          until not instance
+        end
+      end)
+    end
+  end
 end
-for i = 1, 3 do
-game:GetService("ReplicatedStorage").Networking.Remotes.Game.UpgradeGate:FireServer(i)
+local function CollectDrops()
+  for i = 1, 20 do
+    game:GetService("ReplicatedStorage").Networking.Remotes.Drops.CollectDrop:FireServer(i)
+  end
 end
-for i = 1, 20 do
-game:GetService("ReplicatedStorage").Networking.Remotes.Drops.CollectDrop:FireServer(i)
+local function AutoCollect()
+  while getgenv().AutoCollect and task.wait(5) do
+    CollectDrops()
+  end
+end
+local function UpgradeGates()
+  for i = 1, 9 do
+    game:GetService("ReplicatedStorage").Networking.Remotes.Game.UpgradeGate:FireServer(i)
+  end
+end
+local function AutoUpgrade()
+  while getgenv().AutoUpgrade and task.wait(5) do
+    UpgradeGates()
+  end
 end
