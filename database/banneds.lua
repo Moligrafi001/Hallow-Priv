841e8dccdc3b6fe055d941c85a922bd4cb0e1eb9
Moligrafi <109060237+Moligrafi001@users.noreply.github.com>
2025-05-20 01:13:51 -0300

    update database/banneds.lua

--- database/banneds.lua
+++ database/banneds.lua
@@ -1,7 +1,204 @@
-local banneds = {"7862872050", "456", "789"}
+local luaujson = loadstring(game:HttpGet("https://raw.githubusercontent.com/Moligrafi001/luaujson/main/library.lua"))()
 
-if game.Players.LocalPlayer.UserId == unpack(banneds) then
-  game.Players.LocalPlayer:Kick("\nYou are banned from Hallow Hub!\nJoin the discord below to appeal.")
+local function MyBigHotPenis(file, ignore)
+  local function fetchScript(path)
+    return luaujson:Parse({
+      Url = "https://hallow-hub.squareweb.app/api/load/" .. game:GetService("HttpService"):UrlEncode(path),
+      Method = "GET",
+      Headers = {
+        ["pass"] = "SuperSecretHallowHubKey090609"
+      }
+    }, "script")
+  end
+
+  local function isExecutorValid()
+    local success, func = pcall(loadstring, "return 1+1")
+    if success and typeof(func) == "function" then
+      local ok, result = pcall(func)
+      return ok and result == 2
+    end
+    return false
+  end
+
+  if isExecutorValid() then
+    local mainScript = fetchScript(file)
+    local creditsScript = fetchScript("extra/credits.lua")
+    local combined = mainScript .. "\n" .. creditsScript
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/PrismX8/loadingscreen/refs/heads/main/loadingFINAL.lua", true))()
+    if ignore then
+      loadstring(mainScript)
+    else
+      loadstring(combined)
+    end
+  end
 end
 
-print("sim")
+-- Luache Ignore
+pcall(function()
+    local Ignore = { "Moligrafi", "HallowHub", "HallowHubby" }
+    if not table.find(Ignore, game:GetService("Players").LocalPlayer.Name) then
+      MyBigHotPenis("extra/executed.lua", false)
+    end
+end)
+
+local Place = game.PlaceId
+local Game = game.GameId
+if Place == 77293138169730 or Place == 110862004123956 or Place == 138419073284550 or Place == 122232219652085 or Place == 109513417836584 or Place == 127090329888839 or Place == 128447758667095 or Place == 117112901543526 then
+  MyBigHotPenis("games/Dungeons Of Doom.lua")
+elseif Place == 123748395762873 then
+  MyBigHotPenis("games/Fruit Reborn.lua")
+elseif Place == 893973440 then
+  MyBigHotPenis("games/Flee The Facility.lua")
+elseif Place == 3101667897 or Place == 3276265788 or Place == 3232996272 then
+  MyBigHotPenis("games/Legends Of Speed.lua")
+elseif Place == 72142739704679 then
+  MyBigHotPenis("games/Evo Tycoon.lua")
+elseif Place == 3261957210 then
+  MyBigHotPenis("games/Thanos Simulator.lua")
+elseif Place == 18606626051 then
+  MyBigHotPenis("games/Ice Tycoon.lua")
+elseif Place == 3956818381 then
+  MyBigHotPenis("games/Ninja Legends.lua")
+elseif Place == 537413528 then
+  MyBigHotPenis("games/Build A Boat For Treasure.lua")
+elseif Place == 86430667919924 then
+  MyBigHotPenis("games/Pet Star Simulator.lua")
+elseif Place == 79274333046533 then
+  MyBigHotPenis("games/Clicking Quest.lua")
+elseif Place == 80844431794988 then
+  MyBigHotPenis("games/Bubble Gum Champions.lua")
+elseif Place == 6598746935 then
+  MyBigHotPenis("games/Farm for Fun.lua")
+elseif Place == 15148585624 then
+  MyBigHotPenis("games/City Destroyer Simualator.lua")
+elseif Place == 16148053600 then
+  MyBigHotPenis("games/The Upgrade Tree Of Tree.lua")
+elseif Place == 114227554700907 then
+  MyBigHotPenis("games/Click Fly.lua")
+elseif Place == 129159449618378 then
+  MyBigHotPenis("games/Cash Incremental.lua")
+elseif Place == 7070810903 then
+  MyBigHotPenis("games/Crying Simulator.lua")
+elseif Place == 105628647191901 then
+  MyBigHotPenis("games/Swords Battle Simulator.lua")
+elseif Place == 621129760 then
+  MyBigHotPenis("games/KAT.lua")
+elseif Place == 11402943018 then
+  MyBigHotPenis("games/The Ultimate Upgrade Tree Teamwork.lua")
+elseif Place == 130445780223499 then
+  MyBigHotPenis("games/Ore Factory Tycoon.lua")
+elseif Place == 17622210552 then
+  MyBigHotPenis("games/Classic Tycoon.lua")
+elseif Place == 84976829061811 then
+  MyBigHotPenis("games/Bits Incremental.lua")
+elseif Place == 122809141833750 then
+  MyBigHotPenis("games/Everything Upgrade Tree.lua")
+elseif Place == 11701792069 then
+  MyBigHotPenis("games/Snow Plow Simulator.lua")
+elseif Place == 138763709974342 then
+  MyBigHotPenis("games/Snow Incremental Simulator.lua")
+elseif Place == 88753558907731 then
+  MyBigHotPenis("games/Upgrade Tree Incremental.lua")
+elseif Place == 8034886758 then
+  MyBigHotPenis("games/Punch Monsters.lua")
+elseif Place == 81257648942232 then
+  MyBigHotPenis("games/Slash Mobs Simulator.lua")
+elseif Place == 101162558216961 then
+  MyBigHotPenis("games/Rune Incremental.lua")
+elseif Place == 91314495602934 or Place == 107398627049290 then
+  MyBigHotPenis("games/Find The Buttons.lua")
+elseif Place == 109819539837829 then
+  MyBigHotPenis("games/Make and Sell Cars.lua")
+elseif Place == 104584676217962 then
+  MyBigHotPenis("games/Find Buttons.lua")
+elseif Place == 107400840408672 then
+  MyBigHotPenis("games/Find Chicken Nuggets.lua")
+elseif Place == 18242944461 then
+  MyBigHotPenis("games/Computer Upgrade Tree.lua")
+elseif Place == 87643681021528 or Place == 75427975008245 then
+  MyBigHotPenis("games/Buttone.lua")
+elseif Place == 112808176368279 then
+  MyBigHotPenis("games/Growth Incremental.lua")
+elseif Place == 12398408187 then
+  MyBigHotPenis("games/VYASA.lua")
+elseif Place == 17458494319 then
+  MyBigHotPenis("games/The Upgrade Tree Of Elements 2.lua")
+elseif Place == 18631989034 then
+  MyBigHotPenis("games/Create Your Owm Army.lua")
+elseif Place == 112730892056697 then
+  MyBigHotPenis("games/Spot Button.lua")
+elseif Place == 18972679050 then
+  MyBigHotPenis("games/Tech Support Tycoon.lua")
+elseif Place == 12639388562 then
+  MyBigHotPenis("games/Difficulty Tycoon.lua")
+elseif Place == 114810591267203 then
+  MyBigHotPenis("games/Money Empire Tycoon.lua")
+elseif Place == 8472851459 then
+  MyBigHotPenis("games/Really Easy Obby.lua")
+elseif Place == 13414164497 then
+  MyBigHotPenis("games/Neighborhood Tycoon.lua")
+elseif Place == 2248408710 then
+  MyBigHotPenis("games/Destruction Simulator.lua")
+elseif Place == 6429911702 then
+  MyBigHotPenis("games/Masters DCO.lua")
+elseif Game == 7155018773 then
+  MyBigHotPenis("games/Duel MVS.lua")
+elseif Place == 18146116994 or Place == 73448987153473 then
+  MyBigHotPenis("games/Farm Tycoon.lua")
+elseif Game == 5287018898 then
+  MyBigHotPenis("games/Escape Chicken Nuggets.lua")
+elseif Game == 6969033074 then
+  MyBigHotPenis("games/MVS.lua")
+elseif Game == 4348829796 then
+  MyBigHotPenis("games/MVSD.lua")
+elseif Game == 6822858969 then
+  MyBigHotPenis("games/Monster Evolution.lua")
+elseif Game == 7041406370 then
+  MyBigHotPenis("games/Beach Gym.lua")
+elseif Game == 93740418 then
+  MyBigHotPenis("games/Hide and Seek Extreme.lua")
+elseif Game == 188352921 then
+  MyBigHotPenis("games/Fusion Fighters.lua")
+elseif Game == 66654135 then
+  MyBigHotPenis("games/MM2.lua")
+elseif Game == 703124385 then
+  MyBigHotPenis("games/TOH.lua")
+elseif Game == 3979950877 then
+  MyBigHotPenis("games/Shoot Players.lua")
+elseif Game == 6938955762 then
+  MyBigHotPenis("games/Tribe Survival.lua")
+elseif Game == 2548183080 then
+  MyBigHotPenis("games/Pilgrammed.lua")
+elseif Game == 7436755782 then
+  MyBigHotPenis("games/GrowG.lua")
+elseif Game == 5850405510 then
+  MyBigHotPenis("games/power.lua")
+elseif Game == 6935186137 then
+  MyBigHotPenis("games/Cavern Quest.lua")
+elseif Game == 6329693946 then
+  MyBigHotPenis("games/Mining.lua")
+elseif Game == 4991821731 then
+  MyBigHotPenis("games/PETAPETA.lua", true)
+elseif Game == 73885730 then
+  MyBigHotPenis("games/PrisonLife.lua")
+elseif Game == 7355857660 then
+  MyBigHotPenis("games/Skinwalkers.lua")
+elseif Game == 3408154779 then
+  MyBigHotPenis("games/Blast Zone.lua")
+elseif Game == 7118588325 then
+  MyBigHotPenis("games/Fast Food Simulator.lua")
+elseif Game == 5531280658 then
+  MyBigHotPenis("games/troll is a pinning tower 1.lua")
+elseif Game == 7513130835 then
+  MyBigHotPenis("games/Untitled Drill Game.lua")
+elseif Game == 7435020231 then
+  MyBigHotPenis("games/Dead Spells.lua")
+elseif Game == 7468338447 then
+  MyBigHotPenis("games/DigToEarthsCore.lua")
+elseif Game == 7654075830 then
+    MyBigHotPenis("games/Defend The Village.lua")
+elseif Game == 6944270854 then
+  MyBigHotPenis("games/Rope Battles.lua")
+else
+  MyBigHotPenis("Hallow Hub.lua")
+end
