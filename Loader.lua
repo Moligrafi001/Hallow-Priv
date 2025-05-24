c3575255dd6c60adf0d2321a07e52f5d9f54ba33
Moligrafi <109060237+Moligrafi001@users.noreply.github.com>
2025-05-24 11:03:00 -0300

    update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -213,7 +213,7 @@ elseif Place == 2248408710 then
   MyBigHotPenis("games/Destruction Simulator.lua")
 elseif Place == 6429911702 then
   MyBigHotPenis("games/Masters DCO.lua")
-elseif Game == 7155018773 then
+elseif Game == 7219654364 then
   MyBigHotPenis("games/Duel MVS.lua")
 elseif Place == 18146116994 or Place == 73448987153473 then
   MyBigHotPenis("games/Farm Tycoon.lua")
