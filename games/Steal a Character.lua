0b662cdbc717d6956d4df8c3685a127ef6fac912
Moligrafi <109060237+Moligrafi001@users.noreply.github.com>
2025-05-25 18:25:25 -0300

    update games/Steal a Character.lua

--- games/Steal a Character.lua
+++ games/Steal a Character.lua
@@ -1,5 +1,15 @@
+local Rayfield = loadstring(game:HttpGet("https://sirius.menu/rayfield"))()
+local Window = Rayfield:CreateWindow({
+  Name = "Hallow Hub | Steal a Character",
+  Icon = 17091459839,
+  LoadingTitle = "Hallow Hub",
+  LoadingSubtitle = "By Moligrafi",
+  Theme = "Amethyst"
+})
+
 -- Global Values
 getgenv().AutoCollect = false
+getgenv().AutoLock = false
 
 -- Locals
 local eu = game:GetService("Players").LocalPlayer
@@ -18,7 +28,12 @@ end
 -- Functions
 local function AutoCollect()
   while getgenv().AutoCollect and task.wait(1) do
-    
+    for _, platform in pairs(Settings.Studio.Platforms:GetChildren()) do
+      if platform:FindFirstChildOfClass("Model") then
+        firetouchinterest(eu.Character.HumanoidRootPart, platform.Collect, 0)
+        firetouchinterest(eu.Character.HumanoidRootPart, platform.Collect, 1)
+      end
+    end
   end
 end
 local function AutoLock()
@@ -30,15 +45,30 @@ local function AutoLock()
   end
 end
 
-for _, studio in pairs(workspace.Studios:GetChildren()) do
-if studio:GetAttribute("Owner") == game.Players.LocalPlayer.UserId then
-print(studio.Name)
-if studio:GetAttribute("Locked") == false then
-end
-end
-end
+--[[
 game:GetService("Players").LocalPlayer.leaderstats.Worth.Value
 workspace.Studios:GetChildren()[9].LockOneMin.Hitbox.TouchInterest
 workspace.Studios:GetChildren()[9].Platforms:GetChildren()[3].Collect.TouchInterest
 workspace.Studios:GetChildren()[9].Platforms:GetChildren()[3].Noob
 workspace.Studios.Studio.Barrier.Invisibleace.Studios
+--]]
+
+-- Menu
+local Menu = Window:CreateTab("Main", "home")
+Section = Menu:CreateSection("Auto Farm")
+Toggle = Menu:CreateToggle({
+  Name = "Auto Collect Money",
+  CurrentValue = false,
+  Callback = function(Value)
+    getgenv().AutoKill = AutoCollect
+    AutoCollect()
+  end
+})
+Toggle = Menu:CreateToggle({
+  Name = "Auto Lock Base",
+  CurrentValue = false,
+  Callback = function(Value)
+    getgenv().AutoKill = AutoLock
+    AutoLock()
+  end
+})
