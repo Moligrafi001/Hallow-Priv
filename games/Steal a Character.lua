27b5b43b1d6ab2175da4cf329350f0d297358e84
Moligrafi <109060237+Moligrafi001@users.noreply.github.com>
2025-05-25 15:44:19 -0300

    update games/Steal a Character.lua

---
+++ games/Steal a Character.lua
@@ -0,0 +1,13 @@
+for _, studio in pairs(workspace.Studios:GetChildren()) do
+if studio:GetAttribute("Owner") == game.Players.LocalPlayer.UserId then
+print(studio.Name)
+if studio:GetAttribute("Locked") == false then
+end
+end
+end
+game:GetService("Players").LocalPlayer.leaderstats.Worth.Value
+workspace.Studios:GetChildren()[9].LockOneMin.Hitbox.TouchInterest
+workspace.Studios:GetChildren()[9].Platforms:GetChildren()[3].Collect.TouchInterest
+workspace.Studios:GetChildren()[9].Platforms:GetChildren()[3].Noob
+workspace.Studios.Studio.Barrier.Invisibleace.Studios
+7577218041
