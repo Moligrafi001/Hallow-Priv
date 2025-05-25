2d7042fa9a3c70a6427e97bdebadf98f2d026276
Moligrafi <109060237+Moligrafi001@users.noreply.github.com>
2025-05-25 18:17:09 -0300

    update games/Steal a Character.lua

--- games/Steal a Character.lua
+++ games/Steal a Character.lua
@@ -1,3 +1,35 @@
+-- Global Values
+getgenv().AutoCollect = false
+
+-- Locals
+local eu = game:GetService("Players").LocalPlayer
+local Settings = {
+  Studio = nil
+}
+
+-- Loading
+for _, studio in pairs(workspace.Studios:GetChildren()) do
+  if studio:GetAttribute("Owner") == eu.UserId then
+    Settings.Studio = studio
+    break
+  end
+end
+
+-- Functions
+local function AutoCollect()
+  while getgenv().AutoCollect and task.wait(1) do
+    
+  end
+end
+local function AutoLock()
+  while getgenv().AutoLock and task.wait(1) do
+    if not Settings.Studio:GetAttribute("Locked") and eu.leaderstats.Worth.Value >= 30000 then
+      firetouchinterest(eu.Character.HumanoidRootPart, Settings.Studio.LockOneMin.Hitbox, 0)
+      firetouchinterest(eu.Character.HumanoidRootPart, Settings.Studio.LockOneMin.Hitbox, 0)
+    end
+  end
+end
+
 for _, studio in pairs(workspace.Studios:GetChildren()) do
 if studio:GetAttribute("Owner") == game.Players.LocalPlayer.UserId then
 print(studio.Name)
@@ -10,4 +42,3 @@ workspace.Studios:GetChildren()[9].LockOneMin.Hitbox.TouchInterest
 workspace.Studios:GetChildren()[9].Platforms:GetChildren()[3].Collect.TouchInterest
 workspace.Studios:GetChildren()[9].Platforms:GetChildren()[3].Noob
 workspace.Studios.Studio.Barrier.Invisibleace.Studios
-7577218041
